63627467c5d60007077da04348f5ca0ef8f3b9de
Elkhan911 <955pro@mail.ru>
2022-08-31 04:46:58 +0500

    added arr of win combinations

--- script.js
+++ script.js
@@ -2,19 +2,40 @@ let columnsAll = document.querySelectorAll(".table__column");
 
 let playerTurn = true;
 
+let firstPlayerMoves = [];
+let secondPlayerMoves = [];
+
 function crossOrToe() {
-  console.log(playerTurn);
   for (let column of columnsAll) {
     column.addEventListener("click", function () {
       if (playerTurn == true) {
         this.textContent = "X";
         playerTurn = false;
+        let a = column.getAttribute("data-value");
+        firstPlayerMoves.push(a);
+        console.log(firstPlayerMoves);
       } else {
         this.textContent = "O";
         playerTurn = true;
+        let b = column.getAttribute("data-value");
+        secondPlayerMoves.push(b);
+        console.log(secondPlayerMoves);
       }
     });
   }
 }
 
 crossOrToe();
+
+let winCombs = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function isVictory() {}
